4d0193936d0eb9fcb2c7178fd66c7f22aa833431
nakhawa1108 <aknakhawa1108@gmail.com>
2023-07-08 13:22:41 +0530

    doc

---
+++ doc.py
@@ -0,0 +1,6 @@
+def a():
+    '''    git status checks the status.
+    git add adds the file to the temporary file storage.
+    git commit stores file to the local storage.
+    git push origin main pushes file to the remote github.'''
+print(a.__doc__)
